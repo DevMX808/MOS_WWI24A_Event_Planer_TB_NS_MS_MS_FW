d467e65979043cafa3064c0792392dfd502eb171
DevMX808 <mx-dev@outlook.com>
2025-11-05 20:23:38 +0100

    Implement event creation and listing functionality

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Das ist ein Test!")
+var events = [];
+var form = document.getElementById("eventForm");
+var titleInput = document.getElementById("titleInput");
+var dateInput = document.getElementById("dateInput");
+var descInput = document.getElementById("descInput");
+var eventList = document.getElementById("eventList");
+
+function renderEvents() {
+    eventList.innerHTML = "";
+    events.forEach(function(evt) {
+        var li = document.createElement("li");
+        li.innerHTML = "<strong>" + evt.title + "</strong> - " + evt.date + "<br>" + evt.description;
+        eventList.appendChild(li);
+    });
+}
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var newEvent = {
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descInput.value
+    };
+    events.push(newEvent);
+    renderEvents();
+    form.reset();
+});
